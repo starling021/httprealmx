21c399902caa19751b6b812ffbcd7e9529c7c2ef
crinny <42045258+crinny@users.noreply.github.com>
2019-03-31 15:59:51 +0300

    Add files via upload

---
+++ plugins/Example.py
@@ -0,0 +1,15 @@
+from httprealm import BasePlugin
+
+
+class Plugin(BasePlugin):
+    url = ['']  # URLs which plugin will listen to (* to listen to all of them)
+    name = 'Example'
+    version = '1.0'
+
+    def on_load(self, arguments):
+        print('Hello from example plugin! You started HTTPRealm with these arguments:', arguments)
+
+    def on_call(self, url, params, response):
+        print(url)
+#        response = response.replace('<MaxStackablePotions>6</MaxStackablePotions>', '<MaxStackablePotions>60</MaxStackablePotions>')
+        return response  # This response will be returned to client
